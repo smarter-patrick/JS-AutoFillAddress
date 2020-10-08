3e979637d52fdcc4b34a2d23164ddd806fbfbbb3
patrick chungyampin <52589222+smarter-patrick@users.noreply.github.com>
2020-10-08 11:08:58 +1100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/*Add the JavaScript here for the function billingFunction().  It is responsible for setting and clearing the fields in Billing Information */function billingFunction() {
+  if(document.getElementById('same').checked )
+      {
+        x = document.getElementById('shippingName');
+        document.getElementById('billingName').value= x.value;
+        y = document.getElementById('shippingZip');
+        document.getElementById('billingZip').value= y.value;     
+      }
+  else {
+    document.getElementById('billingName').value="";       
+    document.getElementById('billingZip').value="";           
+  }
+} 
