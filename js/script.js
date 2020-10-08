1c1e5478482f6af79b8013f74ea00ca720534b54
patrick chungyampin <52589222+smarter-patrick@users.noreply.github.com>
2020-10-08 11:04:37 +1100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+function showProperties(element){
+  document.getElementById('message').innerHTML = element.alt;
+}
+
+
+function showclick() {
+	document.getElementById('message').innerHTML= 'buddy';
+}
+
+function overclick() {
+	document.getElementById('message').innerHTML='over';
+}
+
+function dropclick() {
+	document.getElementById('message').innerHTML='leave';
+}
